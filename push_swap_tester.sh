b7472556dc6f895ab8b2436e92720affcd8e40fb
Roger de Moura <rde-mour@student.42sp.org.br>
2024-03-16 12:41:28 -0300

    refactor: limits of execution and style

--- push_swap_tester.sh
+++ push_swap_tester.sh
@@ -1,207 +1,154 @@
 #!/bin/bash
 
-if  ! [[ $1 =~ ^[0-9]+$ ]];
-then
-	echo -e "\033[1;97m$0 <valor>\n$0 100\033[0m";
-	exit 1;
-fi
+blue() { echo -ne "\033[1;94m$1\033[0m"; }
+green() { echo -ne "\033[1;92m$1\033[0m"; }
+red() { echo -ne "\033[1;91m$1\033[0m"; }
+purple() { echo -ne "\033[1;95m$1\033[0m"; }
 
-if [ ! -e $0 ];
-then 
-	echo -e "\n\033[1;91m./checker_linux binary not found.\033[0m";
+if  ! [[ $1 =~ ^[0-9]+$ ]] || ! [[ $2 =~ ^[0-9]+$ ]];
+then
+	red "$0 <valor> <repetitions>\n$0 100 2\n";
+	exit 1;
+elif [ ! -e $0 ];
+then
+	red './checker_linux binary not found.';
 	exit 1;
 elif [ ! -e "../push_swap" ];
 then
 	make -C ../ --no-print-directory;
 	if [ ! -e "../push_swap" ];
 	then
+		red './push_swap binary not found.';
 		exit 1;
 	fi
-fi
-
-if [ ! -e "../push_swap" ];
-then
-    echo -e "\n\033[1;91m./push_swap binary not found.\033[0m"
-    exit 1
-fi
-
-echo -e "\n\033[1;95mPUSH SWAP TESTER\033[0m"
-
-if [ $1 -a !SIZE ];
-then
-	SIZE=$1;
 else
-	SIZE=0;
-fi
-
-if [ $2 -a !REPEAT ];
-then
-	REPEAT=$2;
-else
-	REPEAT=0;
-fi
-
-if [ $5 -a !LARGEST ];
-then
-	LARGEST=$5;
-else
-	LARGEST=0;
-fi
-
-if [ $6 -a !SMALLEST ];
-then
-	SMALLEST=$6;
-else
-	SMALLEST=2147483647;
-fi
-
-export VAR="$(seq -10000 10000 | shuf -n $SIZE | tr '\n' ' ')"
-
-../push_swap $VAR > tmp.log
-
-print() {
-	echo -ne "\033[1;97m$1: \033[1;93m$2  \033[1;m"
-}
-
-SA=$(cat tmp.log | grep -w 'sa'| wc -l)
-SB=$(cat tmp.log | grep -w 'sb'| wc -l)
-SS=$(cat tmp.log | grep -w 'ss'| wc -l)
-PA=$(cat tmp.log | grep -w 'pa'| wc -l)
-PB=$(cat tmp.log | grep -w 'pb'| wc -l)
-RA=$(cat tmp.log | grep -w 'ra'| wc -l)
-RB=$(cat tmp.log | grep -w 'rb'| wc -l)
-RR=$(cat tmp.log | grep -w 'rr'| wc -l)
-RRA=$(cat tmp.log | grep -w 'rra'| wc -l)
-RRB=$(cat tmp.log | grep -w 'rrb'| wc -l)
-RRR=$(cat tmp.log | grep -w 'rrr'| wc -l)
-TOTAL=$(($(cat tmp.log | wc -l)))
-
-if [ $SA -gt 0 ];
-then
-	print 'SA' $SA
-fi
-
-if [ $SB -gt 0 ];
-then
-	print 'SB' $SB
-fi
-
-if [ $SS -gt 0 ];
-then
-	print 'SS' $SB
-fi
-
-if [ $PA -gt 0 ];
-then
-	print 'PA' $PA
-fi
-
-if [ $PB -gt 0 ];
-then
-	print 'PB' $PB
-fi
-
-if [ $RA -gt 0 ];
-then
-	print 'RA' $RA
-fi
-
-if [ $RB -gt 0 ];
-then
-	print 'RB' $RB
-fi
-
-if [ $RR -gt 0 ];
-then
-	print 'RR' $RR
-fi
-
-if [ $RRA -gt 0 ];
-then
-	print 'RRA' $RRA
-fi
-
-if [ $RB -gt 0 ];
-then
-	print 'RRB' $RRB
-fi
-
-if [ $RRR -gt 0 ];
-then
-	print 'RRR' $RRR
-fi
-
-echo -e ""
-print 'SIZE' $SIZE
-
-print 'MOVES' $TOTAL
-
-if [ -z "$MEDIAN" ];
-then
-	export MEDIAN=$TOTAL
-else
-	export MEDIAN=$(expr $TOTAL + $MEDIAN)
-fi
-
-if [ $TOTAL -gt $LARGEST ];
-then
-	LARGEST=$TOTAL;
-fi
-
-if [ $TOTAL -lt $SMALLEST ];
-then
-	SMALLEST=$TOTAL
-fi
-
-print 'SMALLEST' $SMALLEST
-print 'LARGEST' $LARGEST
-
-echo -ne "\033[1mCHECKER: \033[0m";
-STATUS=$(../push_swap $VAR | ./checker_linux $VAR)
-if [[ $STATUS == "OK" || $STATUS == "" ]];
-then
-	echo -ne "\033[1;92mOK\033[0m";
-else
-	echo -ne "\033[1;91mKO\033[0m";
-fi
-
-echo -ne "\033[1m AVALIATION: ";
-if ([ $SIZE -lt 101 ] && [ $TOTAL -lt 701 ]) || ([ $SIZE -lt 501 ] && [ $TOTAL -lt 5500 ]);
-then
-	echo -ne "\033[1;92mSUCCESS\033[0m";
-else 
-	if [ $SIZE -lt 501 ];
+	purple "PUSH SWAP TESTER\n";
+	
+	if [ $1 -a !SIZE ];
 	then
-		echo -ne "\033[1;91mFAIL\033[0m";
-		echo -e $VAR >> fail.log;
+		SIZE=$1;
 	else
-		echo -ne "\033[1;94mNOT INFORMED\033[0m";
+		SIZE=0;
 	fi
+	
+	if [ $2 -a !REPEAT ];
+	then
+		REPEAT=$2;
+	else
+		REPEAT=0;
+	fi
+	
+	if [ $5 -a !LARGEST ];
+	then
+		LARGEST=$5;
+	else
+		LARGEST=0;
+	fi
+	
+	if [ $6 -a !SMALLEST ];
+	then
+		SMALLEST=$6;
+	else
+		SMALLEST=2147483647;
+	fi
+	
+	export VAR="$(seq -10000 10000 | shuf -n $SIZE | tr '\n' ' ')"
+	
+	../push_swap $VAR > tmp.log
+	
+	print() {
+		if [[ $2 != "" ]] && [ $2 -gt 0 ];
+		then
+			echo -ne "\033[1m$1: \033[1;93m$2  \033[0m"
+		fi
+	}
+
+	title() { echo -ne "\033[1m$1: \033[0m"; }
+	move() { echo -n $(cat tmp.log | grep -w $1 | wc -l); }
+
+	TOTAL=$(cat tmp.log | wc -l)
+	
+	print 'SA' $(move 'sa');
+	print 'SB' $(move 'sb');
+	print 'SS' $(move 'ss');
+	print 'PA' $(move 'pa');
+	print 'PB' $(move 'pb');
+	print 'RA' $(move 'ra');
+	print 'RB' $(move 'rb');
+	print 'RR' $(move 'rr');
+	print 'RRA' $(move 'rra');
+	print 'RRB' $(move 'rrb');
+	print 'RRR' $(move 'rrr');
+	
+	echo -e ""
+	print 'SIZE' $SIZE
+	print 'MOVES' $TOTAL
+	
+	if [ -z $MEDIAN ];
+	then
+		export MEDIAN=$TOTAL
+	else
+		export MEDIAN=$(expr $TOTAL + $MEDIAN)
+	fi
+	
+	if [ $TOTAL -gt $LARGEST ];
+	then
+		LARGEST=$TOTAL;
+	fi
+	
+	if [ $TOTAL -lt $SMALLEST ];
+	then
+		SMALLEST=$TOTAL
+	fi
+	
+	print 'SMALLEST' $SMALLEST
+	print 'LARGEST' $LARGEST
+	
+	title 'CHECKER';
+	STATUS=$(../push_swap $VAR | ./checker_linux $VAR)
+	if [[ $STATUS == "OK" || $STATUS == "" ]];
+	then
+		green 'OK ';
+	else
+		red 'KO ';
+	fi
+	
+	title 'AVALIATION';
+	if ([ $SIZE -lt 101 ] && [ $TOTAL -lt 701 ]) || ([ $SIZE -lt 501 ] && [ $TOTAL -lt 5500 ]);
+	then
+		green 'SUCCESS';
+	else 
+		if [ $SIZE -lt 501 ];
+		then
+			red 'FAIL';
+			echo -e $VAR >> fail.log;
+		else
+			blue 'NOT INFORMED';
+		fi
+	fi
+	echo "";
+	
+	if [ -z $TIMES ];
+	then
+		export TIMES=1;
+	else
+		export TIMES=$(expr $TIMES + 1)
+	fi
+	
+	print 'MEDIAN' $(expr $MEDIAN / $TIMES);
+	echo "";
+	
+	if [ $REPEAT -gt 1 ];
+	then
+		REPEAT=$(expr $REPEAT - 1);
+		sleep 1;
+	elif [ $REPEAT -gt 0 ];
+	then
+		rm tmp.log;
+		exit 0;
+	else
+		sleep 1;
+	fi
+	
+	$0 $SIZE $REPEAT $MEDIAN $TIMES $LARGEST $SMALLEST
 fi
-echo "";
-
-if [ -z "$TIMES" ];
-then
-	export TIMES=1;
-else
-	export TIMES=$(expr $TIMES + 1)
-fi
-
-print 'MEDIAN' $(expr $MEDIAN / $TIMES);
-echo -e "";
-
-sleep 1
-
-if [ $SIZE -lt 2 ];
-then
-	exit 0;
-fi
-
-if [ $REPEAT -gt 1 ];
-then
-	REPEAT=$(expr $REPEAT - 1);
-else
-	exit 0;
-fi
-
-$0 $SIZE $REPEAT $MEDIAN $TIMES $LARGEST $SMALLEST
